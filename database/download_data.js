7b5d07843e24633ea061220e95b13fecf5dd7d38
marlotm71 <92908405+marlotm71@users.noreply.github.com>
2022-05-01 17:52:34 +0200

    Fin BigWill

--- database/download_data.js
+++ database/download_data.js
@@ -136,8 +136,8 @@ async function get_multi_ohlcv(exchange, pair_list, tf_list, start_date, exchang
 let exchange = new ccxt.binance({ enableRateLimit: true })
 
 // --- Edit coin list here ---
-pair_list = ["BTC/USDT", "ETH/USDT", 'ADA/USDT', 'XRP/USDT', 'BNB/USDT', 'LINK/USDT', 'LTC/USDT', "DOGE/USDT", "SOL/USDT", "AVAX/USDT", "DOT/USDT", "LUNA/USDT", "MATIC/USDT", "NEAR/USDT", "EGLD/USDT", "XTZ/USDT", "AAVE/USDT", "UNI/USDT", "FTM/USDT", "BCH/USDT"]
-// pair_list = coin_list['ftx_main_list']
+//pair_list = ["BTC/USDT", "ETH/USDT", 'ADA/USDT', 'XRP/USDT', 'BNB/USDT', 'LINK/USDT', 'LTC/USDT', "DOGE/USDT", "SOL/USDT", "AVAX/USDT", "DOT/USDT", "LUNA/USDT", "MATIC/USDT", "NEAR/USDT", "EGLD/USDT", "XTZ/USDT", "AAVE/USDT", "UNI/USDT", "FTM/USDT", "BCH/USDT"]
+pair_list = coin_list['hitbtc_2018']
 
 // --- Edit timeframe list and start date here ---
 timeframe_list = ['1h']
